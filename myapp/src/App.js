ce7232cbd4c04fb93c1f540cc803cc90b550daf2
MarianaLagrava <00219820@uca.edu.sv>
2021-11-19 23:42:42 -0600

    Routing to Login (without functionality)

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Login from './Pages/Login/Login';
+// import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 
 function App() {
   return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Navigate replace to={"/login"} />} />
+    //     <Route path="/login" element={<Login />} />
+    //   </Routes>
+    //   </BrowserRouter>
     <h1>Hola</h1>
   );
 }
