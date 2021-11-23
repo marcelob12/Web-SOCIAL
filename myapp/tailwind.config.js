9e414232d2fc74ceb04c75e658b3078f09a91221
FR0K3 <00174320@uca.edu.sv>
2021-11-22 19:50:36 -0600

    Configuring tailwind

--- myapp/tailwind.config.js
+++ myapp/tailwind.config.js
@@ -1,14 +1,23 @@
 module.exports = {
-  purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors:{
-      background: "#111828",
-      aqua: "#34D099",
+      background: "#EEEEEE",
+      aqua: "#2CBC89",
       skyD: "#3A82F1",
       skyL: "#DBEAFF",
-      gray:"#D1D4DB",
-      white:"#FFFFFF"
+      gray:"#bcbfc4",
+      white:"#FFFFFF",
+      whitie:"#F2F2F2",
+      dark: {
+        700: "#24252A",
+        400: "#484A55"
+      },
+      red:{
+        500: "#DB3E3E",
+        300: "#E15151"
+      }
     },
     fontFamily:{
       heading: ['Good Brush', 'cursive'],
