079fbed925f493859dbef7b45707c4c99ea47017
FR0K3 <00174320@uca.edu.sv>
2021-11-28 21:38:22 -0600

    Fixing comments

--- myapp/src/Components/Comment/Comment.jsx
+++ myapp/src/Components/Comment/Comment.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Comment = ({comment, post}) => {
     return ( 
         <div className="flex gap-3 mb-4">
-            <div className="flex items-center justify-center w-12 rounded-full bg-gradient-to-r from-skyD to-aqua">
+            <div className="flex items-center justify-center w-12 rounded-full bg-gradient-to-r from-skyD to-aqua h-12">
                 <div className="flex items-center justify-center w-5/6 bg-white rounded-full h-5/6">
                    <h3 className="font-bold">{comment.user.username.split("_")[0]}</h3>
                 </div>
